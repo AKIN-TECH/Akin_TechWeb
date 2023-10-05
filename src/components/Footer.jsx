9d54ee8a60ad488b180d8212c5f2f937d5c3bb9d
Hadeel_97 <hadeelbalasmeh97@gmail.com>
2023-10-05 11:51:11 +0300

    adding whatssapp contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,23 @@ const Footer = () => {
               </svg>
               <span className=" text-[15px] ml-[6px]">Instgram</span>
             </a>
-            
+
+            <a
+              href="https://wa.me/0770363106"
+              className=" flex flex-row items-center my-[15px] hover:underline cursor-pointer "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-whatsapp"
+                viewBox="0 0 16 16"
+              >
+                <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558 .064 7.926c0 1.399 .366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326z" />
+              </svg>
+              <span className=" text-[15px] ml-[6px]">whatsapp</span>
+            </a>
             <a
               href="https://www.linkedin.com/company/akin-manzel"
               className=" flex flex-row items-center my-[15px] hover:underline cursor-pointer "
