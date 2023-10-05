03ef344d08fca5fbb81e318cbcf9aa69677cd6d4
Hadeel_97 <hadeelbalasmeh97@gmail.com>
2023-10-05 10:18:10 +0300

    fixing p1

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -5,7 +5,6 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
-
 const FeedbackCard = ({
   index,
   testimonial,
@@ -14,61 +13,58 @@ const FeedbackCard = ({
   company,
   image,
 }) => (
-  <div class="mt-8 text-slate-500	flex flex-wrap justify-center items-center text-center ">
+  <div className="flex flex-wrap justify-center items-center">
    <motion.div
-    variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className="bg-white-300 p-10 rounded-5x5 w-[200px] w-full items-center justify-center text-center"
+    variants={fadeIn("", "spring", index * 0.7, 0.75)}
+    className="bg-black-200 p-5 rounded-3xl xs:w-[400px] w-full justify-center items-center"
    >
-    <p className="text-cyan-200 font-black text-[25px] text-center justify-center items-center">"</p>
+    <p className=" mt-7 flex flex-wrap text-cyan-200 font-black text-[48px] justify-center items-center">"</p>
 
-    <div className="mt-7">
-      <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
-      
-      <div className="mt-5 flex justify-between items-center gap-5 ">
-        <div className="flex-1 flex flex-col">
-          <p className="text-cyan-300 font-medium text-[18px]justify-center text-center items-cneter">
-            <span className="blue-text-gradient"></span> {name}
+    <div className="mt-1 justify-center items-center">
+      <p className="text-white text-[20px] justify-center items-center">{testimonial}</p>
+
+      <div className="mt-7 flex justify-center items-center gap-1 justify-center items-center">
+        <div className=" flex flex-col ">
+          <p className="text-white font-medium text-[16px] justify-center items-center">
+            <span className="blue-text-gradient">@</span> {name}
           </p>
-          <p className="mt-1 text-secondary text-[14px] justify-center text-center irems-center">
+          <p className="mt-2 text-secondary text-[12px] justify-center items-center">
             {designation} of {company}
           </p>
         </div>
+
         <img
           src={image}
           alt={`feedback_by-${name}`}
-          className="w-10 h-10 rounded-full object-cover justify-center items-center text-center"
+          className="w-10 h-10 rounded-full object-cover justify-center items-center"
         />
       </div>
-      
     </div>
    </motion.div>
   </div>
-  
 );
 
 const Feedbacks = () => {
   return (
-    // w-full h-[50px] py-2  fixed shadow-lg   shadow-[#00ECFE]/40  bg-slate-300/30  bg-[#03001417]   backdrop-blur-md z-2//
-    <div class="mt-8 text-slate-500	flex flex-wrap justify-center  items-center text-center">
-    <div className="mb-4 bg-black-100 rounded-[30px]">
+    <div className="mt-12 bg-black-100 rounded-[40px] justify-center items-center">
       <div
-        className={`${styles.padding} bg-#121212 rounded-2xl min-h-[200px]text-center `}
+        className={`${styles.padding} bg-tertiary rounded-2xl justify-center items-center `}
       >
-        <div class="mt-5 flex flex-wrap justify-center text-center  ">
+      <div className="flex flex-wrap justify-center items-center">
         <motion.div variants={textVariant()}>
           
-          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
-          <p className={styles.sectionSubText}>What others say</p>
+           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+           <p className={styles.sectionSubText}>What others say</p>
+         
+          
         </motion.div>
-        </div>
       </div>
-      
-      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-5 `}>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7 justify-center items-center`}>
         {testimonials.map((testimonial, index) => (
           <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
         ))}
       </div>
-    </div>
     </div>
   );
 };
